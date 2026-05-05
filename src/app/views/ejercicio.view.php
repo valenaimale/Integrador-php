86124fc5046ceedcaa1d265d5b8ccef61a882474
valenaimale <valeaimale@gmail.com>
2026-05-05 16:43:08 -0300

    html de ejercicio agregado

---
+++ src/app/views/ejercicio.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>ejercicio</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php require 'parts/header.view.php'; ?>
+    <main>
+        <h2>Ejercicio x</h2>
+        <iframe src="youtube.com/tutorial-abdominales"></iframe><!--para poder reproducir el video de youtube en la misma pagina-->
+        <h3>Descripcion:</h3>
+        <dl>
+            <dt>Dificultad</dt>
+                <dd><img src="/estella.JPG" alt=""></dd>
+                <dd><img src="/estella.JPG" alt=""></dd>
+
+            <dt>Musculos involucrados</dt>
+                <dd>biceps</dd>
+                <dd>triceps</dd>
+                <dd>gemelos</dd>
+
+            <dt>Equipamiento necesario</dt>
+                <dd>mancuernas</dd>
+                <dd>barra</dd>
+        </dl>
+    </main>
+    <?php require 'parts/footer.view.php'; ?>
+</body>    
