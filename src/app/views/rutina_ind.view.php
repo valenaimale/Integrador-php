e6262a98085e3141301bb15eb8b0177cad3a2009
valenaimale <valeaimale@gmail.com>
2026-05-05 15:40:55 -0300

    agregado html de rutina individual

---
+++ src/app/views/rutina_ind.view.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>rutina_ind</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php require 'parts/header.view.php'; ?>
+    <main>
+        <h2>Rutina #X</h2>
+        <p>Entrenador</p>
+        <p>Fecha inicio: 07/04</p>
+        <p>Fecha fin: 12/04</p>
+        <p>Objetivo: reforzar tren superior</p>
+        <ul>
+            <li>
+                <article>
+                    <h3>Entrenamiento 1</h3>
+                    <p>Duracion: 30 min</p>
+                    <p>3 ejercicios</p>
+                    <p>Pecho</p>
+                    <ul>
+                        <li>
+                            <a href="/press-de-banca">Press de banca con barra 4x10</a>
+                            <a href="/youtube.com/prees-de-banca"><img src="/logoyoutube.JPG" alt=""></a>
+                        </li>
+                        <li>
+                            <a href="/press-inclidado">Press inclinado 4x12</a>
+                            <a href="/youtube.com/prees-inclinado"><img src="/logoyoutube.JPG" alt=""></a>
+                        </li>
+                        <li>
+                            <a href="/apertura-de-polea">Aperturas en polea 3x8-10=</a>
+                            <a href="/youtube.com/apertura-de-polea"><img src="/logoyoutube.JPG" alt=""></a>
+                        </li>
+                    </ul>
+                </article>
+            </li>
+            <li>
+                <article>
+                    <h3>Entrenamiento 1</h3>
+                    <p>Duracion: 30 min</p>
+                    <p>3 ejercicios</p>
+                    <p>Pecho</p>
+                    <ul>
+                        <li>
+                            <a href="/press-de-banca">Press de banca con barra 4x10</a>
+                            <a href="/youtube.com/prees-de-banca"><img src="/logoyoutube.JPG" alt=""></a>
+                        </li>
+                        <li>
+                            <a href="/press-inclidado">Press inclinado 4x12</a>
+                            <a href="/youtube.com/prees-inclinado"><img src="/logoyoutube.JPG" alt=""></a>
+                        </li>
+                        <li>
+                            <a href="/apertura-de-polea">Aperturas en polea 3x8-10</a>
+                            <a href="/youtube.com/apertura-de-polea"><img src="/logoyoutube.JPG" alt=""></a>
+                        </li>
+                    </ul>
+                </article>
+            </li>
+        </ul>
+    </main>
+    <?php require 'parts/footer.view.php'; ?>
+</body>
