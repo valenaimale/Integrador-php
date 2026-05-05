caab8246577278f0d5e3af89324537bdc6fe8dbd
valenaimale <valeaimale@gmail.com>
2026-05-05 18:04:00 -0300

    crear cuenta para gimnasio agregado

---
+++ src/app/views/crearcuenta-gym.view.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>crear-cuenta</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/crear-cuenta.css">
+</head>
+
+<body>
+    <?php require 'parts/header.view.php'; ?>
+    <main>
+        <h2>Crear cuenta de gimnasio</h2>
+        <form action="/crearCuenta" method="post">
+            <fieldset>
+                <legend>Datos del gimnasio:</legend>
+                <label for="nombre">Nombre</label>
+                <input type="text" id="nombre" name="nombre" placeholder="ej.: Maria Perez" required>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="ej.:tuemail@email.com" required>
+                <label for="telefono">Telefono (opcional)</label>
+                <input type="tel" id="telefono" name="telefono" placeholder="ej.:+54 011 11111111">
+                <label for="direccion">Direccion</label>
+                <input type="text" id="direccion" name="direccion" placeholder="ej.:Calle Falsa 1234" required>
+                <label for="logo">Agregar logo</label>
+                <input type="file" id="logo" name="logo" accept="image/jpeg, image/png, image/webp">
+                <label for="contraseña">Contraseña</label>
+                <input type="password" id="contraseña" name="contraseña" placeholder="ej.:tucontraseña" required>
+                <label for="ccontraseña">Confirmar contraseña</label>
+                <input type="password" id="ccontraseña" name="ccontraseña" placeholder="ej.:tucontraseña" required>
+            <button type="submit">Crear cuenta de gimnasio</button>
+        </form>
+        <div class="form-link">
+            <p>¿Ya tienes una cuenta? <a href="inicio-sesion.html">Iniciar sesión</a></p>
+        </div>
+    </main>
+    <?php require 'parts/footer.view.php'; ?>
+
+</body>
+
+</html>
